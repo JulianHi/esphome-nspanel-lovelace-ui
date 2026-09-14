486bb0a73cfa831d3dd8b0d7f8a3571780707cff
Julian Hildemann <j.hildemann@quadrat.gmbh>
2026-09-14 19:37:30 +0200

    upload can take time, add time to log

--- components/nspanel_lovelace/nspanel_lovelace_upload.cpp
+++ components/nspanel_lovelace/nspanel_lovelace_upload.cpp
@@ -387,7 +387,7 @@ void NSPanelLovelace::upload_tft(const std::string &url) {
   }
 
   std::string response;
-  ESP_LOGD(TAG, "Waiting for upgrade response");
+  ESP_LOGD(TAG, "Waiting for upgrade response (max 120s)");
   this->recv_ret_string_(response, 120000, true);  // This can take some time to return
 
   // The Nextion display will, if it's ready to accept data, send a 0x05 byte.
